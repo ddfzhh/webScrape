92a24845f8d06b9182d69ab7b23d463b5b03e0f7
Ky Tan <kytanmov@gmail.com>
2023-12-15 11:00:28 -0800

    Changes to be committed:

--- python/webScrape.py
+++ python/webScrape.py
@@ -1,7 +1,15 @@
 import os
+import requests
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("HTML/sample.html"), features = "html.parser")
+html_text = requests.get('https://www.boxofficemojo.com/weekly/?ref_=bo_nb_wey_secondarytab')
+print(html_text)
 
+
+
+
+'''
+soup = BeautifulSoup(open("HTML/sample.html"), features = "html.parser")
 for tag in soup.find_all(True):
     print(tag.name)
+'''
