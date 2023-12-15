47e001e0a660d8fb233486e3bc88d8847030b93c
Ky Tan <kytanmov@gmail.com>
2023-12-15 13:02:24 -0800

    Two Changes Made     new file:   HTML/weekly.html 	modified:   python/webScrape.py

--- python/webScrape.py
+++ python/webScrape.py
@@ -2,8 +2,31 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-html_text = requests.get('https://www.boxofficemojo.com/weekly/?ref_=bo_nb_wey_secondarytab')
-print(html_text)
+#get the website from requests
+
+#html_text = requests.get('https://www.boxofficemojo.com/weekly/?ref_=bo_nb_wey_secondarytab')
+#print(html_text)
+
+#parse it with BeautifulSoup
+soup = BeautifulSoup(open("HTML/weekly.html"), features = "html.parser")
+
+
+#create a dictionary with this 
+
+job = soup.find('table', class_ =
+                "a-bordered a-horizontal-stripes a-size-base a-span12 mojo-body-table mojo-table-annotated scrolling-data-table")
+
+print (job)
+
+
+
+
+
+
+
+
+
+
 
 
 
