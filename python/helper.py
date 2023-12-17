e6b0adbb6cc83b8bdb90d694204711d8a4756287
ddfzhh <128191465+ddfzhh@users.noreply.github.com>
2023-12-16 19:12:15 -0800

    Update helper.py

--- python/helper.py
+++ python/helper.py
@@ -1,13 +1,4 @@
-sender = 'kytanmov@gmail.com'
-receivers = ['ktan5@sva.edu', 'kytan@foxmail.com']
-appPassword = 'psjfdsycvcscpnvd'
-
-
-
-
-'''
 sender = 'sample@email.com'
 receivers = ['sample@email.com', 'sample@email.com']
 appPassword = 'samplePassword'
 
-'''
